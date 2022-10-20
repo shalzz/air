95a54a519c48e19cad672bfe1aa9d1dfbfc06f00
Shaleen Jain <shaleen@jain.sh>
2022-10-20 09:44:03 +0000

    update safe list

--- air.py
+++ air.py
@@ -12,10 +12,10 @@ airdrops = {
     "Curve": "https://gist.githubusercontent.com/nicholashc/f4a34c138087195237556077ea6490d7/raw/bfdf0a9886747dfe3465a2e8ea1bfb02ae0386ac/curve.csv",
     "Digg": "https://gist.githubusercontent.com/nicholashc/c21788b0f0391d0d2d2cdcc44940a0e3/raw/0dbdc85e0ebc4d2aaa7cda49d5aaeb903fef69c7/digg.csv",
     "PoolTogether": "https://gist.githubusercontent.com/shalzz/2b278831b1444983dd5527ec89dfaf4a/raw/46dd3fb6b1389d7e588afdc082ff9c5b3594dc1c/pooltogether.csv",
+    "Safe": "https://raw.githubusercontent.com/safe-global/safe-token/main/user-allocations/safes_tokens.csv",
 }
 
 hop  = "https://raw.githubusercontent.com/hop-protocol/hop-airdrop/master/src/data/finalDistribution.csv"
-safe = "https://gist.githubusercontent.com/tschubotz/f4f346afc6ca495ad3a53f4b58dba5dc/raw/75514fa612ec549a79905e621137b87e46dcf20b/safe_user_allocations.csv"
 
 my_addrs = [addr.lower() for addr in re.findall(r"0x\w{40}", open("addrs.txt").read())]
 for drop, url in airdrops.items():
@@ -26,10 +26,5 @@ for drop, url in airdrops.items():
 
 print("Hop")
 for addr, _, _, _, _, amount in csv.reader(requests.get(hop).text.splitlines()):
-    if addr.lower() in my_addrs:
-        print(addr, amount)
-
-print("Safe")
-for addr, _, _, _, _, amount in csv.reader(requests.get(safe).text.splitlines()):
     if addr.lower() in my_addrs:
         print(addr, amount)
